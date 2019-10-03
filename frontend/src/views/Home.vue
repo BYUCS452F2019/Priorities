da867a88c3ff72e33b27f1eac432e537883fa130
Zack Mortensen <zack.morty@gmail.com>
2019-10-03 10:59:07 -0600

    add basic frontend and backend

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Priorities</h1>
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'home',
+  data: function(){
+    return {
+      msg: ''
+      }
+  },
+  created: function() {
+    let vm = this
+    this.$http.get('http://127.0.0.1:8081').then(function (response) {
+      vm.msg = response.data
+    })
+  }
+}
+</script>
