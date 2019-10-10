46e468e73260d5aa0dc864b92d6e94609d67cfaf
Zack Mortensen <zack.morty@gmail.com>
2019-10-10 12:57:30 -0600

    change backend structure, add gitignore file, add config files for backend and frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import json from './config.js'
 
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+Vue.mixin({
+  methods: {
+    getBackendURL: function () {
+      return json.backend
+    }
+  }
+})
 
 new Vue({
   router,
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,19 +2,19 @@
   <div class="home">
     <h1>Welcome to Priorities</h1>
     <p>{{msg}}</p>
+    <p>{{getBackendURL()}}</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 export default {
   name: 'home',
-  data: function(){
+  data: function () {
     return {
       msg: ''
       }
   },
-  created: function() {
+  created: function () {
     let vm = this
     this.$http.get('http://127.0.0.1:8081').then(function (response) {
       vm.msg = response.data
