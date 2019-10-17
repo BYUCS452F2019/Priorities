862b04cb07958d440441448c81406ebeff41d50a
Zack Mortensen <zack.morty@gmail.com>
2019-10-17 14:49:08 -0600

    fix error in UserModel

--- backend/src/main/java/com/springbok/priorities/models/UserModel.java
+++ backend/src/main/java/com/springbok/priorities/models/UserModel.java
@@ -1,13 +1,18 @@
 package com.springbok.priorities.models;
 
 public class UserModel {
-    public String name;
+    public String username;
     public String email;
     public String password;
 
-    public UserModel(String name, String email, String password) {
-        this.name = name;
+    public UserModel(String username, String email, String password) {
+        this.username = username;
         this.email = email;
+        this.password = password;
+    }
+
+    public UserModel(String username, String password) {
+        this.username = username;
         this.password = password;
     }
 }
