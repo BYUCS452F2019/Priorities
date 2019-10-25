a25517954e1110748bec211573d22bad9ccaf4f5
zmorty <zack.morty@gmail.com>
2019-10-25 14:50:53 -0600

    add sql dependency and daoManager

--- backend/src/main/java/com/springbok/priorities/dao/UserDao.java
+++ backend/src/main/java/com/springbok/priorities/dao/UserDao.java
@@ -1,13 +1,47 @@
 package com.springbok.priorities.dao;
 
-import com.springbok.priorities.models.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.springbok.priorities.models.UserModel;
 
 public class UserDao {
     public static String create(UserModel model) {
+
         return "authorization";
     }
 
     public static String read(UserModel model) {
         return "logged in";
     }
+
+    public static List<String> getFieldTypesFromModel(UserModel model) {
+        List<String> types = new ArrayList<String>();
+        types.add("email");
+        types.add("username");
+        types.add("password");
+        return types;
+    }
+
+    public static List<String> getFieldValuesFromModel(UserModel model) {
+        List<String> types = new ArrayList<String>();
+        if (!model.email.isEmpty()) {
+            types.add(model.email);
+        } else {
+            types.add("null");
+        }
+        if (!model.username.isEmpty()) {
+            types.add(model.username);
+        } else {
+            types.add("null");
+        }
+        if (!model.password.isEmpty()) {
+            types.add(model.password);
+        } else {
+            types.add("null");
+        }
+        return types;
+    }
+
+    
 }
