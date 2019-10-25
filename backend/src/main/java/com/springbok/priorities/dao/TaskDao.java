40b50114bb8c4ac5a4bf3af02a7f3fb61d7b7ee2
Eric Schofield <eschos24@gmail.com>
2019-10-25 16:14:09 -0600

    Added sql for TaskDao

--- backend/src/main/java/com/springbok/priorities/dao/TaskDao.java
+++ backend/src/main/java/com/springbok/priorities/dao/TaskDao.java
@@ -1,10 +1,23 @@
 package com.springbok.priorities.dao;
 
-import com.springbok.priorities.models.*;
+import java.sql.*;
+
+import com.springbok.priorities.models.TaskModel;
 
 public class TaskDao {
     public static String create(TaskModel task) {
-        return "authorization";
+        try {
+            Connection conn = DaoManager.getConnection();
+            Statement stmt = conn.createStatement();
+            String sql = "INSERT into task (user_id, priority_id, title, descrition, completed, creation_date, due_date) VALUES ("
+                        + task.userID + ", " + task.priorityID + ", " + task.title + ", " + task.description + ", " + task.completed + ", "
+                        + task.creationDate + ", " + task.dueDate;
+
+            DaoManager.closeConnection(conn);
+            return "authorization";
+        } catch (Exception sQLException) {
+            return "bad connection";
+        }
     }
 
     public static String getTasksForUserID(String userID) {
