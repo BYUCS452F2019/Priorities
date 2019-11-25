ddee5cc7c23c7b33bc51edb245ebf334d1387b09
Zack Mortensen <zack.morty@gmail.com>
2019-11-25 16:29:33 -0700

    add curly bracket

--- backend/src/main/java/com/springbok/priorities/command/mongodao/MongoUserDao.java
+++ backend/src/main/java/com/springbok/priorities/command/mongodao/MongoUserDao.java
@@ -2,6 +2,6 @@ package com.springbok.priorities.command.mongodao;
 
 import com.springbok.priorities.command.daofactory.UserDaoInterface;
 
-public class MongoUserDao implements UserDaoInterface
+public class MongoUserDao implements UserDaoInterface {
     
 }
