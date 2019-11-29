23b6577d343154be9c37741e0a1cfdb0854f45a5
Zack Mortensen <zack.morty@gmail.com>
2019-11-28 20:25:06 -0700

    start mongoDB priority stuff

---
+++ backend/src/main/java/com/springbok/priorities/mongodao/MongoDaoManager.java
@@ -0,0 +1,32 @@
+package com.springbok.priorities.mongodao;
+
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+
+import java.util.Arrays;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;;
+
+public class MongoDaoManager {
+    public static MongoCollection<Document> getUserCollection() {
+        final String databaseHost = "localhost";
+        
+        MongoCredential credential = MongoCredential.createCredential("userName", "database", "password".toCharArray());
+
+        MongoClient mongoClient = MongoClients.create(
+            MongoClientSettings.builder()
+                .applyToClusterSettings(builder -> 
+                    builder.hosts(Arrays.asList(new ServerAddress(databaseHost))))
+                .credential(credential)
+                .build());
+
+        return mongoClient.getDatabase("databaseName").getCollection("user");
+    }
+
+}
--- backend/src/main/java/com/springbok/priorities/mongodao/MongoPriorityDao.java
+++ backend/src/main/java/com/springbok/priorities/mongodao/MongoPriorityDao.java
@@ -1,28 +1,59 @@
 package com.springbok.priorities.mongodao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.client.MongoCollection;
+import static com.mongodb.client.model.Filters.*;
+import org.bson.Document;
+import com.mongodb.client.model.Updates;
 
 import com.springbok.priorities.daofactory.PriorityDaoInterface;
 import com.springbok.priorities.models.PriorityModel;
 
 public class MongoPriorityDao implements PriorityDaoInterface{
-    
+
     @Override
     public List<PriorityModel> getPrioritiesForUserID(String userID) {
-        // TODO Auto-generated method stub
-        return null;
+        MongoCollection<Document> coll = MongoDaoManager.getUserCollection();
+
+        String priorityJson = (String)coll.find(and(eq("user_id", userID))).first().get("priorities");
+        
+        Gson gson = new Gson();
+        List<PriorityModel> prioritiesList = gson.fromJson(priorityJson, new TypeToken<List<PriorityModel>>(){}.getType());
+        return prioritiesList;
     }
 
     @Override
     public Integer create(PriorityModel priority) {
-        // TODO Auto-generated method stub
-        return null;
+        MongoCollection<Document> coll = MongoDaoManager.getUserCollection();
+
+        Integer priority_id = (int) coll.count(and(eq("user_id", priority.getUser_id().toString())));
+
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("priority_id", priority_id);
+        map.put("user_id", priority.getUser_id());
+        map.put("title", priority.getTitle());
+        map.put("type", priority.getType());
+        map.put("number", priority.getNumber());
+
+        Document document = new Document(map);
+    
+        coll.insertOne(document);
+        return priority_id;
     }
 
     @Override
     public Boolean updatePriority(PriorityModel priority) {
-        // TODO Auto-generated method stub
-        return null;
+
+        return true;
     }
 
 }
