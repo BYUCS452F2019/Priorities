e65a05887da30b1f754d783d46f9cbd5ed314a4b
Zack Mortensen <zack.morty@gmail.com>
2019-12-03 20:33:08 -0700

    change date to timestamp

--- backend/src/main/java/com/springbok/priorities/sqldao/SQLDaoManager.java
+++ backend/src/main/java/com/springbok/priorities/sqldao/SQLDaoManager.java
@@ -75,7 +75,11 @@ public class SQLDaoManager {
         Class<?> boxed = boxPrimitiveClass(type);// box if primitive(Point 6)
         value = boxed.cast(value);
       }
+      System.out.println(type);
+      System.out.println(value);
       if (type == boolean.class) {
+        System.out.println(type);
+        System.out.println(value);
         if ((int)value == 0) {
           value = false;
         } else if ((int) value == 1) {
