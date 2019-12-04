df51decddfa4203c3cd6cc1ac273ab7f013169da
rbundy <reid.bundy.il@gmail.com>
2019-12-03 17:21:48 -0700

    mongo user dao implemented

--- backend/src/main/java/com/springbok/priorities/mongodao/MongoUserDao.java
+++ backend/src/main/java/com/springbok/priorities/mongodao/MongoUserDao.java
@@ -3,24 +3,69 @@ package com.springbok.priorities.mongodao;
 import com.springbok.priorities.daofactory.UserDaoInterface;
 import com.springbok.priorities.models.UserModel;
 
+import com.mongodb.client.MongoCollection;
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Updates.*;
+
+import com.mongodb.Block;
+import org.bson.Document;
+
 public class MongoUserDao implements UserDaoInterface {
+
+    private static final String USER_COLLECTION = "user_collection";
 
     @Override
     public Integer create(UserModel model) {
-        // TODO Auto-generated method stub
-        return null;
+        MongoCollection<Document> coll = MongoDaoManager.getCollection(USER_COLLECTION);
+
+        Integer user_id = (int) coll.countDocuments();
+
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("userId", user_id);
+        map.put("username", model.getUsername());
+        map.put("email", model.getEmail());
+        map.put("password", model.getPassword());
+
+        Document document = new Document(map);
+    
+        coll.insertOne(document);
+        return user_id;
     }
 
     @Override
     public Integer getUserID(String user_name, String password) {
-        // TODO Auto-generated method stub
-        return null;
+        UserModel user;
+
+        MongoCollection<Document> coll = MongoDaoManager.getCollection(USER_COLLECTION);
+        coll.find(and(eq("username", user_name), eq("password"), password)).forEach(new Block<Document>() {
+            @Override
+            public void apply(Document t) {
+                return (Integer) t.get("user_id");
+            }
+        });
+
+        throw new Exception("Could not find user with username and password");
     }
 
     @Override
     public UserModel getUser(String user_id) {
-        // TODO Auto-generated method stub
-        return null;
+        UserModel user;
+
+        MongoCollection<Document> coll = MongoDaoManager.getCollection(USER_COLLECTION);
+        coll.find(and(eq("user_id", user_id)).forEach(new Block<Document>() {
+            @Override
+            public void apply(Document t) {
+                user = new UserModel(
+                    (Integer) t.get("user_id"),
+                    (String)  t.get("username"),
+                    (String) t.get("email"),
+                    (String) t.get("password")
+                ));
+                return t.get("user_id");
+            }
+        });
+        return user;
     }
 
     @Override
