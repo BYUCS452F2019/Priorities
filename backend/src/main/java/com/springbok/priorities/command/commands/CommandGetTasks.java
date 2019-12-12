a5d24252090992e0932e77d579b56793455f3559
Zack Mortensen <zack.morty@gmail.com>
2019-12-12 09:51:08 -0700

    fix parsing string to timestamp

--- backend/src/main/java/com/springbok/priorities/command/commands/CommandGetTasks.java
+++ backend/src/main/java/com/springbok/priorities/command/commands/CommandGetTasks.java
@@ -13,7 +13,7 @@ public class CommandGetTasks implements CommandInterface {
     public CommandGetTasks(Map<String, Object> commandData) {
       this.user_id = (Double)commandData.get("user_id");
       if (commandData.containsKey("date")) {
-        this.date = new Timestamp (Long.parseLong((String)commandData.get("date")));
+        this.date =  new Timestamp(Long.parseLong((String)commandData.get("date")));
       }
     }
 
